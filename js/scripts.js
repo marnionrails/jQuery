81de908417a5de075fb44f7daa10f7b2840e5d6d
Marni <suchermarni@gmail.com>
2020-09-07 16:38:14 -0400

    add JQuery functions to change background

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
   $("h1").click(function() {
     alert("This is a header.");
+    alert("Try again");
   });
 
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 
   $("img").click(function() {
     alert("This is a paragraph.");
+  });
+});
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
   });
 });
