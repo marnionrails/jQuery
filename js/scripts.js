c7f45e5a8d2addb2c12ec430e06730e3d182d0cf
Marni <suchermarni@gmail.com>
2020-09-07 16:18:54 -0400

    enclose 'Click here' in span.clickable'

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   });
 
   $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
 
   $("img").click(function() {
