556275645104a517cecd1ce0479e737b889a0112
Marni <suchermarni@gmail.com>
2020-09-06 23:38:21 -0400

    add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const jquery351 = require("./jquery-3.5.1");
+
+$("h1").click(function() {
+  alert("This is a header.");
+});
+
+$("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+$("img").click(function() {
+  alert("This is a paragraph.");
+});
